d0102b81c9093fc5a0b44df11e838711649152c3
Aleksey Chepurko <aleksey.chepurko@gmail.com>
2017-05-15 06:30:13 +0300

    pure webpack build with react (without font loading)

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Алексей on 15.05.2017.
+ */
+import './style/main.sass';
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <div className="">hello react</div>,
+    document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
+
+const buildPath = "build";
+//TODO NODE_ENV check to propd
+const isProd = process.env.NODE_ENV ? true : false;
+const cssDev = ['style-loader','css-loader','sass-loader'];
+const cssProd = ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: ['css-loader','sass-loader'],
+    publicPath: buildPath
+});
+const cssConfig = isProd  ? cssProd : cssDev;
+// console.log(process.env.NODE_ENV == "production");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, buildPath),
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.sass$/,
+                use: cssConfig
+            },
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+            //TODO static dir check in regexp
+            //TODO prod build with build dir
+            {test: /\.(jpe?g|png|gif|svg)$/,
+                use: ['file-loader?name=/images/[hash].[ext]',
+                      'image-webpack-loader'
+                ]}
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Stiv",
+            template: "./src/index.html",
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true
+        }),
+        new ExtractTextPlugin({
+            filename: "style.css",
+            allChunks: true,
+            disable: !isProd
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, buildPath),
+        compress: true,
+        stats: "errors-only",
+        open: true,
+        hot: true
+    },
+
+};
